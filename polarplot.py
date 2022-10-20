951a514e27def4915c14323a41a65c899e688063
Corrosive <coltonbruni@Coltons-MacBook-Pro.local>
2022-10-20 00:08:39 -0500

    multiple subplots and abstraction

--- polarplot.py
+++ polarplot.py
@@ -1,19 +1,19 @@
+from sklearn.preprocessing import normalize
+import matplotlib.pyplot as plt
 import numpy as np
-from properpolar import *
 import threading
+import multiprocessing
 
-def plot(value, mm):
+def get_results(value):
     AOI = np.arange(20,36,1) #angles of incidence
-    azi = np.arange(0,90,2)
+    azi = np.arange(0,92,2) #azimuth
     results = []
     azimuth = azi
     for i in range(len(AOI)):
         angle = AOI[i]
         txt = "my_resultsMM_NSL_hexagonal_lattice_radius_37.5_nm_structure_thickness_50_nm_pitch_120_wavelength_2.1e-07nm_" + str(angle) + "AOI.txt"
-        lamsi = np.loadtxt(txt, usecols=[3,value])
+        lamsi = np.loadtxt("results/" + txt, usecols=[3,value-1])
         results.append(lamsi)
-
-
 
     y = []
     for i in range(len(AOI)):
@@ -25,43 +25,44 @@ def plot(value, mm):
 
     X,Y = np.meshgrid(np.radians(x),y)
 
-    fig, ax = plt.subplots(figsize=(8,6),subplot_kw=dict(projection='polar'))
-
-
-    n=len(azimuth)
-    m=len(AOI)
+    n = len(azimuth)
+    m = len(AOI)
     z = np.zeros((m,n))
-    print(azimuth)
-    for j in range(len(azimuth)):
-        for i in range(len(AOI)):
-            z[i][j] = results[i][j][1]
+    for j in range(len(AOI)):
+        for i in range(len(azimuth)): 
+            z[j][i] = results[j][i][1]
+    z = z/abs(np.amin(z))
+    return [X, Y, z] 
 
+def plot(X, Y, Z, mm):
+
+    rTicks = [20,25,30,35] 
+    xTicks = [0,np.pi/12,np.pi/6,np.pi/4,np.pi/3,np.pi/2] 
+
+    fig, axis = plt.subplots(4,4,figsize=(10,10),subplot_kw=dict(projection='polar'))
     cm = plt.cm.seismic
-    fig=ax.pcolormesh(X,Y,z,vmin=-0.8, vmax=0.8, shading='gouraud', antialiased=True, cmap = cm)
-
-    cb = plt.colorbar(fig)
-    plt.title(mm,fontsize=20)
-    ax.set_rticks([20,30,40,45])
-    ax.set_xticks([0,np.pi/12,np.pi/6,np.pi/4,np.pi/3,np.pi/2])
-    ax.set_rlim(0, 45)
-    ax.set_xlim(0, np.pi/2)
-    ax.grid( color = 'gray', linestyle = '--', linewidth = 1 )
-    #plot1.axes.get_yaxis().set_visible(False)
+    for j in range(4):
+        for i in range(4):
+            val = i*4+j
+            axis[i,j].pcolormesh(X[val],Y[val],Z[val],vmin=-0.8, vmax=0.8, shading='gouraud', antialiased=True, cmap = cm) 
+            axis[i,j].set_rticks(rTicks, fontsize=10) #AOI
+            axis[i,j].set_xticks(xTicks, fontsize=10)
+            axis[i,j].set_rlim(rTicks[0], rTicks[-1])
+            axis[i,j].set_xlim(xTicks[0], xTicks[-1]) #azimuth
+            axis[i,j].grid( color = 'gray', linestyle = '--', linewidth = 1 )
+            axis[i,j].set_title(mm[val],fontsize=20)
+    plt.tight_layout(h_pad=1)
+    plt.savefig('constant_wvl_210.png')
     plt.show()
 
-thread1 = threading.Thread(target=plot(7,11))
-thread12 = threading.Thread(target=plot(8,12))
-thread13 = threading.Thread(target=plot(9,13))
-thread14 = threading.Thread(target=plot(10,14))
-thread21 = threading.Thread(target=plot(11,21))
-thread22 = threading.Thread(target=plot(12,22))
-thread23 = threading.Thread(target=plot(13,23))
-thread24 = threading.Thread(target=plot(14,24))
-thread31 = threading.Thread(target=plot(15,31))
-thread32 = threading.Thread(target=plot(16,32))
-thread33 = threading.Thread(target=plot(17,33))
-thread34 = threading.Thread(target=plot(18,34))
-thread41 = threading.Thread(target=plot(19,41))
-thread42 = threading.Thread(target=plot(20,42))
-thread43 = threading.Thread(target=plot(21,43))
-thread44 = threading.Thread(target=plot(22,44))
+if __name__ == '__main__':
+    results = [[],[],[],[]]
+    for i in range(4):
+        for j in range(4):
+            data = get_results(7+i*4+j)
+            for r in range(3):
+                results[r].append(data[r])
+            results[3].append(int(str(i+1) + str(j+1)))
+
+    plot(results[0],results[1],results[2], results[3])
+
